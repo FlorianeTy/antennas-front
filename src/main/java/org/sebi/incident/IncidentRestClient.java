07e5097b9cbef74146e07e1c0cf8e02154a8e997
Patrick <patrick.douchement@capgemini.com>
2022-05-19 16:13:54 +0200

    FallbackHandler

--- src/main/java/org/sebi/incident/IncidentRestClient.java
+++ src/main/java/org/sebi/incident/IncidentRestClient.java
@@ -1,5 +1,8 @@
 package org.sebi.incident;
 
+import org.eclipse.microprofile.faulttolerance.ExecutionContext;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.FallbackHandler;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.GET;
@@ -12,5 +15,16 @@ import java.util.List;
 public interface IncidentRestClient {
     @GET
     @Path("/incidents")
+    @Fallback(DefaultIncident.class)
     List<Incident> getIncidents(@QueryParam("api_key") String apiKey);
+
+    class DefaultIncident implements FallbackHandler<Incident> {
+
+        @Override
+        public Incident handle(ExecutionContext executionContext) {
+            Incident i = new Incident();
+            i.description = "fallback";
+            return i;
+        }
+    }
 }
