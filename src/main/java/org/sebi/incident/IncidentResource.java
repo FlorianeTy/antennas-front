f053fe621aa41dc050e7617786d777a32405735b
Patrick <patrick.douchement@capgemini.com>
2022-05-19 14:38:30 +0200

    Openshift c'est null

--- src/main/java/org/sebi/incident/IncidentResource.java
+++ src/main/java/org/sebi/incident/IncidentResource.java
@@ -1,15 +1,25 @@
 package org.sebi.incident;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-@Path("rest/incidents")
+@Path("rest")
 public class IncidentResource {
     
-    @GET
+    @RestClient
+    IncidentRestClient incidentRestClient;
+
+    @ConfigProperty(name = "APIKEY", defaultValue = "secret")
+    String apiKey;
+
+    @GET()
+    @Path("incidents")
     public List<Incident> getIncidents(){
-       return Incident.listAll();
-    } 
+        return incidentRestClient.getIncidents(apiKey);
+    }
 }
